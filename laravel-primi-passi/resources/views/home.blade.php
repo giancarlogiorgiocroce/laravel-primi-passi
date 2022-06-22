377ac0de809c7792fb9b5cf30bcaf646c78e9107
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-22 17:29:18 +0200

    Esercizio completato (ma è normale che devo pushare 82 elementi?)

---
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Esercizio - Laravel primi passi</title>
+    <style>
+        div{
+            width: 80%;
+            margin: 0 auto;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <h1>Hello Framework World!</h1>
+    </div>
+</body>
+</html>
